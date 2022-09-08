50e145c787c44ae99fc4c7195f5b54a62a808f63
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-08 00:45:22 -0300

    feat: Add auth mutations and inputs

--- modules/graphql/schema.ts
+++ modules/graphql/schema.ts
@@ -4,6 +4,23 @@ const schema = gql`
   type Query {
     _dummy: String
   }
+
+  type Mutation {
+    register(credentials: RegisterMutationInput!): String
+    login(credentials: LoginMutationInput!): String
+  }
+
+  input RegisterMutationInput {
+    email: String!
+    username: String!
+    password: String!
+    confirmPassword: String!
+  }
+
+  input LoginMutationInput {
+    email: String!
+    password: String!
+  }
 `;
 
 export default schema;

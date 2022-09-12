a0f876903fabd29e4165cb75f7980751b8a7e2d2
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-12 15:31:54 -0300

    feat: Add recipes and trending mutations + types

--- modules/graphql/schema.ts
+++ modules/graphql/schema.ts
@@ -1,13 +1,39 @@
 import { gql } from "apollo-server-core";
 
 const schema = gql`
+  scalar DateTime
+
   type Query {
-    _dummy: String
+    recipes(query: RecipesQueryInput!): [Recipe]
+    trending(time: TrendingTime!, pagination: Pagination): [Recipe]
   }
 
   type Mutation {
     register(credentials: RegisterMutationInput!): String
     login(credentials: LoginMutationInput!): String
+  }
+
+  type User {
+    id: String
+    username: String
+    thumbnail: String
+    createdOn: DateTime
+  }
+
+  type Recipe {
+    id: ID
+    author: User
+    title: String
+    thumbnail: String
+    country: String
+    content: String
+    portions: Int
+    cooking_time: Int
+    isVegan: Boolean
+    glutenFree: Boolean
+    createdOn: DateTime
+    likes: Int
+    likedByUser: Boolean
   }
 
   input RegisterMutationInput {
@@ -20,6 +46,57 @@ const schema = gql`
   input LoginMutationInput {
     email: String!
     password: String!
+  }
+
+  input DateRange {
+    before: DateTime
+    after: DateTime
+  }
+
+  input RecipeQueryFields {
+    title: String
+    country: String
+    portions: Int
+    isGlutenFree: Boolean
+    isVegan: Boolean
+    author: String
+    createdOn: DateRange
+  }
+
+  input RecipeSorting {
+    by: RecipeSortBy
+    order: SortDirections
+  }
+
+  input Pagination {
+    offset: Int = 0
+    take: Int = 5
+  }
+
+  input RecipesQueryInput {
+    values: RecipeQueryFields
+    sort: RecipeSorting
+    pagination: Pagination
+  }
+
+  enum SortDirections {
+    ASC
+    DESC
+  }
+
+  enum RecipeSortBy {
+    LIKES
+    PORTIONS
+    COUNTRY
+    TITLE
+    CREATED_ON
+  }
+
+  enum TrendingTime {
+    YESTERDAY
+    LAST_WEEK
+    LAST_MONTH
+    LAST_YEAR
   }
 `;
 

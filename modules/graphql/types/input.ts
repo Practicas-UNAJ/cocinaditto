3d3a0d9356299ed560849392d1f7e3de3553bfc5
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-12 15:29:36 -0300

    feat: Add recipes query input type

--- modules/graphql/types/input.ts
+++ modules/graphql/types/input.ts
@@ -1,3 +1,5 @@
+import { Pagination, RecipeQueryFields, RecipeSorting } from "./types";
+
 export interface RegisterMutationInput {
   credentials: {
     email: string;
@@ -11,5 +13,13 @@ export interface LoginMutationInput {
   credentials: {
     email: string;
     password: string;
+  };
+}
+
+export interface RecipesQueryInput {
+  query: {
+    values: RecipeQueryFields;
+    sort: RecipeSorting;
+    pagination: Pagination;
   };
 }

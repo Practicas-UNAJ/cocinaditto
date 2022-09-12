63a513968fd5b1764778d93ebf6eba2b81a58e2b
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-12 15:33:58 -0300

    feat: Add resolvers to server

--- libs/apollo.ts
+++ libs/apollo.ts
@@ -3,10 +3,18 @@ import typeDefs from "../modules/graphql/schema";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import Mutation from "../modules/graphql/resolvers/mutation";
 import { createContext } from "../modules/graphql/context";
+import Query from "../modules/graphql/resolvers/query";
+import Field from "../modules/graphql/resolvers/field";
+import Enum from "../modules/graphql/resolvers/enum";
+import { DateTimeResolver } from "graphql-scalars";
 
 const apollo = new ApolloServer({
   typeDefs,
   resolvers: {
+    DateTime: DateTimeResolver,
+    ...Enum,
+    ...Field,
+    Query,
     Mutation,
   },
   context: createContext,

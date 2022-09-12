2fcb7141ff560247a22921b0c5c0f61a7f986cd1
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-12 15:28:33 -0300

    feat: Add SortDirections and RecipeSortBy enums

---
+++ modules/graphql/types/enum.ts
@@ -0,0 +1,12 @@
+export enum SortDirections {
+  ASC = "asc",
+  DESC = "desc",
+}
+
+export enum RecipeSortBy {
+  LIKES = "likes",
+  PORTIONS = "portions",
+  COUNTRY = "country",
+  TITLE = "title",
+  CREATED_ON = "createdOn",
+}

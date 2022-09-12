1acb78e97e179b82c8ae9805f5cf904f003a1905
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-12 15:30:20 -0300

    feat: Add graphql types typing

---
+++ modules/graphql/types/types.ts
@@ -0,0 +1,26 @@
+import { RecipeSortBy, SortDirections } from "./enum";
+
+export type RecipeSorting = {
+  by: RecipeSortBy;
+  order: SortDirections;
+};
+
+export type Pagination = {
+  offset: number;
+  take: number;
+};
+
+export type RecipeQueryFields = {
+  title: string;
+  country: string;
+  portions: number;
+  isGlutenFree: boolean;
+  isVegan: boolean;
+  author: string;
+  createdOn: DateRange;
+};
+
+export type DateRange = {
+  before: Date;
+  after: Date;
+};

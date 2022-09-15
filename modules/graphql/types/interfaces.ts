d9711e46d46d229b74a011966eb1fc037b4cddf3
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-15 15:34:13 -0300

    feat: Add graphql entities interfaces

---
+++ modules/graphql/types/interfaces.ts
@@ -0,0 +1,22 @@
+export interface User {
+  id: string;
+  username: string;
+  thumbnail: string;
+  createdOn: Date;
+}
+
+export interface Recipe {
+  id: number;
+  author: User;
+  title: string;
+  thumbnail: string;
+  country: string;
+  content: string;
+  portions: number;
+  cooking_time: number;
+  isVegan: boolean;
+  glutenFree: boolean;
+  createdOn: Date;
+  likes: number;
+  likedByUser: boolean;
+}

428e408f5f691665c731f45a4e93bb36b046ffca
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-15 15:37:06 -0300

    refactor: Remove types from queries definition

--- apollo/queries.ts
+++ apollo/queries.ts
@@ -1,48 +1,4 @@
 import { gql } from "@apollo/client";
-import { RecipesQueryInput } from "../modules/graphql/types/input";
-import { Recipe } from "../modules/graphql/types/interfaces";
-import { Pagination, RecipeQueryFields } from "../modules/graphql/types/types";
-
-export enum RecipeQuerySortOrder {
-  ASC = "ASC",
-  DESC = "DESC",
-}
-
-export enum RecipeQuerySortBy {
-  LIKES = "LIKES",
-  PORTIONS = "PORTIONS",
-  COUNTRY = "COUNTRY",
-  TITLE = "TITLE",
-  CREATED_ON = "CREATED_ON",
-}
-
-export interface TrendingRecipesData {
-  trending: Recipe[];
-}
-
-export interface TrendingRecipesVars {
-  time: string;
-  pagination: Pagination;
-}
-
-export interface TrendingRecipesData {
-  trending: Recipe[];
-}
-
-export interface RecipesData {
-  recipes: Recipe[];
-}
-
-export interface RecipesVars {
-  query: {
-    values?: RecipeQueryFields;
-    sort?: {
-      by: RecipeQuerySortBy;
-      order: RecipeQuerySortOrder;
-    };
-    pagination?: Pagination;
-  };
-}
 
 export const TrendingRecipesQuery = gql`
   query Trending($time: TrendingTime!, $pagination: Pagination!) {

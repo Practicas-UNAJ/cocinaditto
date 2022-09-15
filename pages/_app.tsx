6d25899156c6a75b5465a8c7b4973e4e05d580c0
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-15 15:31:39 -0300

    feat: Add Apollo provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,54 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ReactElement, ReactNode } from 'react'
-import { NextPage } from 'next'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+import app from "../libs/firebaseApp";
+import { setContext } from "@apollo/client/link/context";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:3000/api/graphql",
+});
+
+const getIdToken = async () => await app.auth.currentUser?.getIdToken();
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await getIdToken();
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+      "Bypass-Tunnel-Reminder": true,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([authLink, httpLink]),
+  cache: new InMemoryCache(),
+});
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page)
+  const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />)
+  return (
+    <ApolloProvider client={client}>
+      {getLayout(<Component {...pageProps} />)}
+    </ApolloProvider>
+  );
 }

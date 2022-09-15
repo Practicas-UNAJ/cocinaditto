5d5e65e0d0cc9d84cc4213b71e82ec564aa89898
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-15 15:32:20 -0300

    feat: Add recipes query enums

---
+++ apollo/enum.ts
@@ -0,0 +1,12 @@
+export enum RecipeQuerySortOrder {
+  ASC = "ASC",
+  DESC = "DESC",
+}
+
+export enum RecipeQuerySortBy {
+  LIKES = "LIKES",
+  PORTIONS = "PORTIONS",
+  COUNTRY = "COUNTRY",
+  TITLE = "TITLE",
+  CREATED_ON = "CREATED_ON",
+}

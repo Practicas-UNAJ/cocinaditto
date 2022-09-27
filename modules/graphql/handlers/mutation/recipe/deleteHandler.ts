4c68b09ebba84f30cc532d9c7e56be482a14e50a
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-27 19:59:56 -0300

    feat: Add delete recipe handler

---
+++ modules/graphql/handlers/mutation/recipe/deleteHandler.ts
@@ -0,0 +1,35 @@
+import { PrismaClient, Recipe, User } from "@prisma/client";
+import { GraphQLError } from "graphql";
+import { DeleteResult, MutationHandlerFunc } from "../../../types/handlers";
+
+const DeleteRecipe: MutationHandlerFunc<Recipe, DeleteResult> = async (
+  payload: Pick<Recipe, "id">,
+  prisma: PrismaClient,
+  user: User
+) => {
+  try {
+    const recipe = await prisma.recipe.findUnique({
+      where: {
+        id: payload.id,
+      },
+      include: {
+        author: true,
+      },
+    });
+
+    if (!recipe) throw new GraphQLError("Not Found");
+    if (user.id !== recipe.author.id) throw new GraphQLError("Forbidden");
+
+    const deleted = await prisma.recipe.delete({
+      where: {
+        id: payload.id,
+      },
+    });
+
+    return { deleted: Boolean(deleted) };
+  } catch (error) {
+    throw new GraphQLError(JSON.stringify(error));
+  }
+};
+
+export default DeleteRecipe;

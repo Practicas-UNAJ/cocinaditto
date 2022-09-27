881d2f3c05c48ce2b94795bce91c66223354191e
Fabricio Futryk <ffutryk52@gmail.com>
2022-09-27 20:01:00 -0300

    feat: Add Recipe mutation

--- modules/graphql/resolvers/mutation.ts
+++ modules/graphql/resolvers/mutation.ts
@@ -1,9 +1,11 @@
-import { LoginMutationHandler } from "../handlers/mutation/LoginMutationHandler";
+import LoginMutationHandler from "../handlers/mutation/LoginMutationHandler";
+import RecipeMutationHandler from "../handlers/mutation/RecipeMutationHandler";
 import RegisterMutationHandler from "../handlers/mutation/RegisterMutationHandler";
 
 const mutationResolver = {
   register: RegisterMutationHandler,
   login: LoginMutationHandler,
+  recipe: RecipeMutationHandler,
 };
 
 export default mutationResolver;

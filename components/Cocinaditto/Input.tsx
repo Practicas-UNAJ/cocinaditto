f63080cdca2d6bf4b96b78b0c90b9fa396d8a717
01R4D-M <monterosrubendario@gmail.com>
2022-09-28 20:29:00 -0300

    feat: Add HTML input properties

--- components/Cocinaditto/Input.tsx
+++ components/Cocinaditto/Input.tsx
@@ -1,18 +1,20 @@
-import { FunctionComponent, HTMLInputTypeAttribute } from "react"
+import { DetailedHTMLProps, FunctionComponent, InputHTMLAttributes } from "react"
+import { twMerge } from "tailwind-merge";
 
-interface IInputData {
+interface IInputData extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
     label?: string
-    type: HTMLInputTypeAttribute
 }
 
 export const CocinadittoInput: FunctionComponent<IInputData> = ({
     label,
-    type
+    type,
+    placeholder,
+    className,
 }) => {
     return (
         <label className="flex flex-col items-center text-white">
             {label}
-            <input type={type} className="text-black rounded-full p-1 shadow-[inset_0px_4px_4px_rgba(0,0,0,0.25)]" />
+            <input type={type} placeholder={placeholder} className={twMerge("text-black rounded-full p-1 shadow-[inset_0px_4px_4px_rgba(0,0,0,0.25)]", className)} />
         </label>
     )
 }

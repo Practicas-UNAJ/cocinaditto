53523609d9c3691407fa626e8bde7435e2baf02e
01R4D-M <monterosrubendario@gmail.com>
2022-09-28 20:12:07 -0300

    refactor: Add link to main page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,12 @@
 import { NextComponentType } from "next";
+import Link from "next/link";
 
 export const Navbar = () => {
     return (
-        <nav className="flex flex-row justify-center bg-gradient-radial from-primary-600 to-primary-700 shadow shadow-primary-800 w-screen">
-            <h1 className="text-black text-2xl">Cocinaditto</h1>
+        <nav className="flex flex-row justify-center bg-gradient-radial from-primary-500 to-primary-700 shadow shadow-primary-800 w-screen">
+            <Link href="/">
+                <a><h1 className="text-black text-2xl">Cocinaditto</h1></a>
+            </Link>
         </nav>
     )
 }

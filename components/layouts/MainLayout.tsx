b8ac9bf844a5d50e9ea7f4ae63851280e0f81ee6
01R4D-M <monterosrubendario@gmail.com>
2022-09-28 19:39:11 -0300

    refactor: Change css

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -2,9 +2,9 @@ import { Navbar } from "../Navbar"
 
 export const MainLayout = ({children}: any) => {
     return (
-        <div className="flex flex-col items-center overflow-x-hidden">
+        <div className="flex flex-col items-center overflow-x-hidden font-poppins">
             <Navbar/>
-            <section className="flex flex-col gap-10 px-5 w-screen sm:max-w-screen-sm">
+            <section className="flex flex-col gap-10 p-5 w-screen sm:max-w-screen-sm">
                 {children}
             </section>
         </div>

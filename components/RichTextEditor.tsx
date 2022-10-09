af7d102cd51273e5812d4bca716d2ba6c6158ac0
01R4D-M <monterosrubendario@gmail.com>
2022-10-09 16:33:04 -0300

    feat: Add Rich Text Editor component

---
+++ components/RichTextEditor.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
+import dynamic from "next/dynamic";
+
+const Editor = dynamic(
+  () => {
+    return import("react-draft-wysiwyg").then(mod => mod.Editor);
+  },
+  { ssr: false }
+);
+
+interface IRichTextEditorProps {
+  cb: (content: string) => void;
+  initialContent?: string;
+  error?: {
+    message?: string;
+  };
+}
+
+const RichTextEditor = ({
+  cb,
+  initialContent,
+  error,
+}: IRichTextEditorProps) => {
+  const [editorState, setEditorState] = useState(
+    initialContent
+      ? EditorState.createWithContent(
+          convertFromRaw(JSON.parse(initialContent))
+        )
+      : EditorState.createEmpty()
+  );
+
+  const onEditorStateChange = (state: EditorState) => {
+    const currentContent = state.getCurrentContent();
+    const raw = convertToRaw(currentContent);
+    const stringifiedContent = JSON.stringify(raw);
+
+    if (!currentContent.hasText()) cb("");
+    else cb(stringifiedContent);
+    setEditorState(state);
+  };
+
+  return (
+    <>
+      <Editor
+        editorState={editorState}
+        toolbar={{
+          options: ["inline", "list", "textAlign", "remove"],
+        }}
+        onEditorStateChange={onEditorStateChange}
+        wrapperClassName={`${
+          error?.message ? "border-2 border-danger-500 rounded-lg" : ""
+        }`}
+      />
+      {error?.message && (
+        <div className="text-xs font-light text-danger-500">
+          {error.message}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default RichTextEditor;

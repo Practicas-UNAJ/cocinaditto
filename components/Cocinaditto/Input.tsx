8cff0833b686ec9683e69a4ed7f81656e5d3965d
01R4D-M <monterosrubendario@gmail.com>
2022-10-09 16:38:09 -0300

    refactor: Change css

--- components/Cocinaditto/Input.tsx
+++ components/Cocinaditto/Input.tsx
@@ -14,7 +14,7 @@ export const CocinadittoInput: FunctionComponent<IInputData> = ({
     return (
         <label className="flex flex-col items-center text-white">
             {label}
-            <input type={type} placeholder={placeholder} className={twMerge("text-black rounded-full p-1 shadow-[inset_0px_4px_4px_rgba(0,0,0,0.25)]", className)} />
+            <input type={type} placeholder={placeholder} className={twMerge("text-black rounded-full py-1 px-3 shadow-[inset_0px_4px_4px_rgba(0,0,0,0.25)]", className)} />
         </label>
     )
 }

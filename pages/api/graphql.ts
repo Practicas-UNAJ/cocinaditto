8bd2b70f3c8c799985349b13f25c5d8570aee4d0
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-12 14:55:51 -0300

    feat: Add cors options

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,7 +1,16 @@
 import apollo, { startServer } from "../../libs/apollo";
 import Cors from "micro-cors";
 
-const cors = Cors();
+const cors = Cors({
+  origin: "*",
+  allowCredentials: true,
+  allowMethods: ["GET", "POST", "PUT", "DELETE"],
+  allowHeaders: [
+    "access-control-allow-credentials",
+    "access-control-allow-origin",
+    "content-type",
+  ],
+});
 
 export default cors(async function handler(req, res) {
   if (req.method === "OPTIONS") {

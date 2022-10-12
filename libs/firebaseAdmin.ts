9faa82c554e4211892f9bab1719c66c9eec01de8
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-12 04:25:06 -0300

    refactor: Load serviceAccount as base64 from .env

--- libs/firebaseAdmin.ts
+++ libs/firebaseAdmin.ts
@@ -3,7 +3,14 @@ import serviceAccount from "../modules/firebase/serviceAccount.json";
 
 if (!admin.apps.length)
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    credential: admin.credential.cert(
+      JSON.parse(
+        Buffer.from(
+          process.env.FIREBASE_SERVICE_ACCOUNT_BASE64 as string,
+          "base64"
+        ).toString("ascii")
+      ) as admin.ServiceAccount
+    ),
   });
 
 const auth = admin.auth();

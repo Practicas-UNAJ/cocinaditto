3e2b341d93a2203e8db158a998ade59b5b78ce81
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-12 04:12:42 -0300

    feat: Add generation & upload of default thumbnail

--- modules/graphql/handlers/mutation/RegisterMutationHandler.ts
+++ modules/graphql/handlers/mutation/RegisterMutationHandler.ts
@@ -1,8 +1,11 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { RegisterMutationInput } from "../../types/input";
+import { v4 } from "uuid";
 import firebaseAdmin from "../../../../libs/firebaseAdmin";
 import firebaseApp from "../../../../libs/firebaseApp";
 import ApolloContext from "../../types/context";
+import generateImage from "../../../../utils/generateImage";
+import uploadImage from "../../../firebase/uploadImage";
 
 const RegisterMutationHandler = async (
   _parent: any,
@@ -19,10 +22,18 @@ const RegisterMutationHandler = async (
     firebaseCredentials.user.uid
   );
 
+  const image = generateImage(firebaseCredentials.user.uid);
+  const uuid = v4();
+  const result = await uploadImage(
+    image,
+    `users/${firebaseCredentials.user.uid}/${uuid}.png`
+  );
+
   await ctx.prisma.user.create({
     data: {
       username: credentials.username,
       id: firebaseCredentials.user.uid,
+      thumbnail: result.downloadUrl,
     },
   });
 

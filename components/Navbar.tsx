52858dfab4edaea18402acc71e59804b0c29f8d3
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-21 04:28:01 -0300

    refactor: Match css gradient

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,11 +2,13 @@ import { NextComponentType } from "next";
 import Link from "next/link";
 
 export const Navbar = () => {
-    return (
-        <nav className="flex flex-row justify-center bg-gradient-radial from-primary-500 to-primary-700 shadow shadow-primary-800 w-screen">
-            <Link href="/">
-                <a><h1 className="text-black text-2xl">Cocinaditto</h1></a>
-            </Link>
-        </nav>
-    )
-}
+  return (
+    <nav className="flex flex-row justify-center nav-gradient shadow shadow-primary-800 w-screen">
+      <Link href="/">
+        <a>
+          <h1 className="text-black text-2xl font-medium">Cocinaditto</h1>
+        </a>
+      </Link>
+    </nav>
+  );
+};

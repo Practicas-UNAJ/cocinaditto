38ee19a36e2630b6711f3c7c388c5da5f0df1efd
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-21 04:26:10 -0300

    fix: Match new tailwind style

--- components/recipes/RecipeImage.tsx
+++ components/recipes/RecipeImage.tsx
@@ -1,12 +1,15 @@
-import Image from "next/image"
-import { FunctionComponent } from "react"
+import Image from "next/image";
+import { FunctionComponent } from "react";
 
 interface IRecipeImageData {
-    image: string 
+  image: string;
 }
 
-export const RecipeImage: FunctionComponent<IRecipeImageData> = ({image}) => {
-    return (
-        <img src={image} className="object-cover aspect-[5/2] rounded-md border-[1px] border-card-900"/>
-    )
-}
+export const RecipeImage: FunctionComponent<IRecipeImageData> = ({ image }) => {
+  return (
+    <img
+      src={image}
+      className="object-cover aspect-[5/2] rounded-md border-[1px] border-brown-900"
+    />
+  );
+};

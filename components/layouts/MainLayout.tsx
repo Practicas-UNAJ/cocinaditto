87a2f0e3c3a38171ed087c7ac6c01347910ad8b6
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-21 04:29:51 -0300

    feat: Add brown-900 as text color

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,12 +1,12 @@
-import { Navbar } from "../Navbar"
+import { Navbar } from "../Navbar";
 
-export const MainLayout = ({children}: any) => {
-    return (
-        <div className="flex flex-col items-center overflow-x-hidden font-poppins">
-            <Navbar/>
-            <section className="flex flex-col gap-10 p-5 w-screen sm:max-w-screen-sm">
-                {children}
-            </section>
-        </div>
-    )
-}
+export const MainLayout = ({ children }: any) => {
+  return (
+    <div className="flex flex-col items-center overflow-x-hidden font-poppins text-brown-900">
+      <Navbar />
+      <section className="flex flex-col gap-10 p-5 w-screen sm:max-w-screen-sm">
+        {children}
+      </section>
+    </div>
+  );
+};

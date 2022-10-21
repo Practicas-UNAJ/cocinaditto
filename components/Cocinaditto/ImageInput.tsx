3d13e977fffd146e0893af3d637a9b14ba810e34
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-21 04:29:17 -0300

    fix: Match new tailwind styles

--- components/Cocinaditto/ImageInput.tsx
+++ components/Cocinaditto/ImageInput.tsx
@@ -4,19 +4,21 @@ import { RecipeImage } from "../recipes/RecipeImage";
 import placeholder from "../../assets/images/recipe-placeholder.png";
 
 export const ImageInput: NextComponentType = () => {
-    const [imageData, setImageData] = useState<string>("");
+  const [imageData, setImageData] = useState<string>("");
 
-    return (
-        <div className="relative group">
-            {
-                imageData !== "" ?
-                <RecipeImage image="https://picsum.photos/1000"/>
-                : <RecipeImage image={placeholder.src} />
-            }
-            <div className="absolute top-0 left-0 grid items-center justify-center w-full h-full">
-                <input type="file" className="absolute w-full h-full opacity-0" />
-                <span className="text-card-700 font-semibold">{imageData !== "" ? "" : "Publica una imagen de tu receta"}</span>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="relative group">
+      {imageData !== "" ? (
+        <RecipeImage image="https://picsum.photos/1000" />
+      ) : (
+        <RecipeImage image={placeholder.src} />
+      )}
+      <div className="absolute top-0 left-0 grid items-center justify-center w-full h-full">
+        <input type="file" className="absolute w-full h-full opacity-0" />
+        <span className="text-brown-700 font-semibold">
+          {imageData !== "" ? "" : "Publica una imagen de tu receta"}
+        </span>
+      </div>
+    </div>
+  );
+};

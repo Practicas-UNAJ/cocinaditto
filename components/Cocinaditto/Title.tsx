9cc7904593ba98250322c0c4538cdcbf8c065900
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-21 04:31:04 -0300

    refactor: Remove text color

--- components/Cocinaditto/Title.tsx
+++ components/Cocinaditto/Title.tsx
@@ -1,11 +1,13 @@
 import { FunctionComponent } from "react";
 
 interface ITitleProps {
-    text: string
+  text: string;
 }
 
-export const CocinadittoTitle: FunctionComponent<ITitleProps> = ({text}) => {
-    return (
-        <h2 className="text-xl text-card-900 text-center font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">{text}</h2>
-    )
-}
+export const CocinadittoTitle: FunctionComponent<ITitleProps> = ({ text }) => {
+  return (
+    <h2 className="text-xl text-center font-semibold drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+      {text}
+    </h2>
+  );
+};

563f34fa1eebbbba55f1619aea0e3e4b7dec9f18
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-26 02:19:10 -0300

    feat: Add Link

--- components/recipes/RecipeCard.tsx
+++ components/recipes/RecipeCard.tsx
@@ -6,6 +6,7 @@ import insignia2 from "../../assets/images/insignia2.png";
 import insignia3 from "../../assets/images/insignia3.png";
 import GlutenFree from "../../assets/images/gluten-free.png";
 import { Recipe } from "../../modules/graphql/types/interfaces";
+import Link from "next/link";
 
 export enum HIGHLIGHTED {
   NONE = "NONE",
@@ -40,55 +41,57 @@ export const RecipeCard: FunctionComponent<RecipeCardData> = ({
   const [liked, setLiked] = useState<boolean>(likedByUser);
 
   return (
-    <div
-      key={id}
-      className="relative gradient min-w-[9rem] p-2 rounded-xl text-sm shadow-black/25 shadow-md "
-    >
-      <div className="w-5 absolute -translate-x-2/3 -translate-y-2/3">
-        {highlighted !== HIGHLIGHTED.NONE && (
-          <Image
-            src={highlightedObject[highlighted]}
-            objectFit="cover"
-            layout="responsive"
-          />
-        )}
-      </div>
-      <img
-        src={thumbnail}
-        className="w-full rounded-t-xl object-cover aspect-[4/3]"
-      />
-      <span>{title}</span>
-      <div className="flex flex-row justify-between items-center">
-        <div className="flex flex-row gap-1 items-center">
-          <Icon icon="ant-design:clock-circle-filled" />
-          <span>{cooking_time}&apos;</span>
-        </div>
-        {isVegan && <Icon icon="iconoir:vegan" />}
-      </div>
-      <div className="flex flex-row justify-between items-center text">
-        <div className="flex flex-row gap-1 items-center">
-          <Icon icon="fa-solid:pizza-slice" />
-          <span>{portions} Porciones</span>
-        </div>
-        <div className="w-3 h-3">
-          {glutenFree && (
-            <Image src={GlutenFree} objectFit="cover" layout="responsive" />
+    <Link href={`/recipe/${id}`}>
+      <div
+        key={id}
+        className="relative gradient min-w-[9rem] p-2 rounded-xl text-sm shadow-black/25 shadow-md "
+      >
+        <div className="w-5 absolute -translate-x-2/3 -translate-y-2/3">
+          {highlighted !== HIGHLIGHTED.NONE && (
+            <Image
+              src={highlightedObject[highlighted]}
+              objectFit="cover"
+              layout="responsive"
+            />
           )}
         </div>
-      </div>
-      <div className="flex flex-row justify-between items-center">
-        <div className="flex flex-row gap-1 items-center">
-          <button onClick={() => setLiked(!liked)}>
+        <img
+          src={thumbnail}
+          className="w-full rounded-t-xl object-cover aspect-[4/3]"
+        />
+        <div className="text-ellipsis whitespace-nowrap inline-block max-w-full">
+          {title}
+        </div>
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex flex-row gap-1 items-center">
+            <Icon icon="ant-design:clock-circle-filled" />
+            <span>{cooking_time}&apos;</span>
+          </div>
+          {isVegan && <Icon icon="iconoir:vegan" />}
+        </div>
+        <div className="flex flex-row justify-between items-center text">
+          <div className="flex flex-row gap-1 items-center">
+            <Icon icon="fa-solid:pizza-slice" />
+            <span>{portions} Porciones</span>
+          </div>
+          <div className="w-3 h-3">
+            {glutenFree && (
+              <Image src={GlutenFree} objectFit="cover" layout="responsive" />
+            )}
+          </div>
+        </div>
+        <div className="flex flex-row justify-between items-center">
+          <div className="flex flex-row gap-1 items-center">
             {liked ? (
               <Icon icon="icon-park-solid:like" />
             ) : (
               <Icon icon="icon-park-outline:like" />
             )}
-          </button>
-          <span>{likes}</span>
+            <span>{likes}</span>
+          </div>
+          <span>{author.username}</span>
         </div>
-        <span>{author.username}</span>
       </div>
-    </div>
+    </Link>
   );
 };

35a3e519de547df8c9b9986c426c9c80028a2032
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-26 02:18:01 -0300

    feat: Add backend connection

--- pages/recipe/[id].tsx
+++ pages/recipe/[id].tsx
@@ -5,58 +5,64 @@ import { CocinadittoTitle } from "../../components/Cocinaditto/Title";
 import { MainLayout } from "../../components/layouts/MainLayout";
 import { RecipeImage } from "../../components/recipes/RecipeImage";
 import { NextPageWithLayout } from "../_app";
-import veganIcon from "../../assets/images/vegan.png"
-import glutenFreeIcon from "../../assets/images/gluten-free.png"
-import { Icon } from "@iconify/react";
+import veganIcon from "../../assets/images/vegan.png";
+import glutenFreeIcon from "../../assets/images/gluten-free.png";
+import useRecipePage from "../../hooks/useRecipePage";
+import { useRouter } from "next/router";
+import useLike from "../../hooks/useLike";
 
 const Page: NextPageWithLayout = () => {
-    const [isLiked, setIsLiked] = useState<boolean>(false)
+  const router = useRouter();
+  const { recipe, isLoading } = useRecipePage(router.query.id as string);
+  const { icon: likeIcon, likeRecipe } = useLike(
+    router.query.id as string,
+    recipe?.likedByUser!
+  );
 
-    return (
-        <>
-            <Head>
-                <title>chocotorta</title>
-            </Head>
-            <RecipeImage image="https://picsum.photos/1000"/>
-            <CocinadittoTitle text="Titulo de la receta" />
+  return recipe ? (
+    <>
+      <Head>
+        <title>Cocinaditto | {recipe.title}</title>
+      </Head>
+      <RecipeImage image={recipe.thumbnail} />
+      <CocinadittoTitle text={recipe.title} />
 
-            <div className="flex flex-row gap-5 justify-center">
-                <FlagCheckbutton image={glutenFreeIcon} state={true} setState={() => console.log("a")}/>
-                <FlagCheckbutton image={veganIcon} state={true} setState={() => console.log("a")}/>
-            </div>
+      <div className="flex flex-row gap-5 justify-center">
+        {recipe.glutenFree && (
+          <FlagCheckbutton image={glutenFreeIcon} state={true} />
+        )}
+        {recipe.isVegan && <FlagCheckbutton image={veganIcon} state={true} />}
+      </div>
 
-            <div className="bg-card-500 font-light text-card-900 p-4 rounded-2xl shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat quidem illo minus nemo et quia, iste iusto nihil error fuga sunt odit sequi est laborum assumenda corporis veritatis suscipit? Non.
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat quam expedita hic porro ipsam officia unde in, est doloribus nisi, aliquid dignissimos veritatis, laborum voluptates eius. Architecto consequuntur quos necessitatibus!
-            </div>
+      <div className="bg-card-500 font-light text-card-900 p-4 rounded-2xl shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+        {recipe.content}
+      </div>
 
-            <div className="flex flex-row text-card-900 justify-center items-center gap-2">
-                <button onClick={() => setIsLiked(!isLiked)}>
-                    {isLiked ? (
-                    <Icon icon="icon-park-solid:like" />
-                    ) : (
-                    <Icon icon="icon-park-outline:like" />
-                    )}
-                </button>
-                23 likes
-            </div>
+      <div className="flex flex-row text-card-900 justify-center items-center gap-2">
+        <button onClick={() => likeRecipe()}>{likeIcon}</button>
+        {recipe.likes}
+      </div>
 
-            <div className="flex flex-row gap-3 justify-center">
-                <button className="rounded-full font-semibold p-4 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-danger-600 text-white">Eliminar</button>
-                <button className="rounded-full font-semibold p-4 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-secondary-500 text-card-900">Editar</button>
-            </div>
-        </>
-    )
-}
+      <div className="flex flex-row gap-3 justify-center">
+        <button className="rounded-full font-semibold p-4 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-danger-600 text-white">
+          Eliminar
+        </button>
+        <button className="rounded-full font-semibold p-4 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-secondary-500 text-card-900">
+          Editar
+        </button>
+      </div>
+    </>
+  ) : (
+    <></>
+  );
+};
 
 Page.getLayout = function getLayout(page: ReactElement) {
-    return (
-        <>
-            <MainLayout>
-                {page}
-            </MainLayout>
-        </>
-    )
-}
-  
+  return (
+    <>
+      <MainLayout>{page}</MainLayout>
+    </>
+  );
+};
+
 export default Page;

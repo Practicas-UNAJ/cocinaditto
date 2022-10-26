98da8cf22cfb57dda34d051fb43a16e8294be8d3
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-26 02:18:51 -0300

    refactor: Change setState type

--- components/Cocinaditto/FlagCheckbutton.tsx
+++ components/Cocinaditto/FlagCheckbutton.tsx
@@ -3,15 +3,23 @@ import { Dispatch, FunctionComponent, SetStateAction, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface IFlagCheckbuttonData {
-    image: StaticImageData
-    state: boolean,
-    setState: Dispatch<SetStateAction<boolean>>
+  image: StaticImageData;
+  state: boolean;
+  setState?: () => void;
 }
 
-export const FlagCheckbutton: FunctionComponent<IFlagCheckbuttonData> = (props) => {
-    return (
-        <div className={twMerge("cursor-pointer rounded-full shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-8 h-8 p-1", props.state ? "bg-success-200" : "bg-secondary-500")} onClick={() => props.setState(!props.state)}>
-            <Image src={props.image} layout="responsive" objectFit="contain" />
-        </div>
-    )
-}
+export const FlagCheckbutton: FunctionComponent<IFlagCheckbuttonData> = (
+  props
+) => {
+  return (
+    <div
+      className={twMerge(
+        "cursor-pointer rounded-full shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-8 h-8 p-1",
+        props.state ? "bg-success-200" : "bg-secondary-500"
+      )}
+      onClick={props.setState}
+    >
+      <Image src={props.image} layout="responsive" objectFit="contain" />
+    </div>
+  );
+};

d50e5af6a95aaedaf6a16989481640eeedd5ae97
Fabricio Futryk <ffutryk52@gmail.com>
2022-10-26 02:20:55 -0300

    feat: Add recipe & random handler to resolver

--- modules/graphql/resolvers/query.ts
+++ modules/graphql/resolvers/query.ts
@@ -1,9 +1,13 @@
+import RandomHandler from "../handlers/query/RandomQueryHandler";
+import RecipeQueryHandler from "../handlers/query/RecipeQueryHandler";
 import RecipesQueryHandler from "../handlers/query/RecipesQueryHandler";
 import TrendingQueryHandler from "../handlers/query/TrendingQueryHandler";
 
 const Query = {
   recipes: RecipesQueryHandler,
+  recipe: RecipeQueryHandler,
   trending: TrendingQueryHandler,
+  random: RandomHandler,
 };
 
 export default Query;

336d77a954d8a618112480f63d0e3edb187964a4
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-09 16:41:01 -0300

    feat: Add IAuthContext

--- interfaces/context.ts
+++ interfaces/context.ts
@@ -1,7 +1,17 @@
 import { EModals } from "../enums/modals";
+import { UserCredential } from "firebase/auth";
+import { IUser } from "./user";
 
 export interface IModalContext {
   modal: EModals | null;
   unsetModal: () => void;
   setModal: (modal: EModals) => void;
+}
+
+export interface IAuthContext {
+  currentUser: IUser | null;
+  signIn: (token: string) => Promise<UserCredential>;
+  signOut: () => any;
+  signUp: (token: string) => Promise<UserCredential>;
+  fetch: () => Promise<void>;
 }

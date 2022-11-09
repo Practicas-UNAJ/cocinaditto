f5756140fdef85556d1f935184968f2f1a545bbf
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-09 16:43:11 -0300

    feat: Consume AuthProvider and initialize Firebase App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,8 @@ import app from "../libs/firebaseApp";
 import { setContext } from "@apollo/client/link/context";
 import ModalProvider from "../context/ModalProvider";
 import Head from "next/head";
+import AuthProvider from "../context/AuthProvider";
+import { getApps, initializeApp } from "firebase/app";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -22,6 +24,16 @@ export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
+
+if (!getApps().length)
+  initializeApp({
+    apiKey: `${process.env.NEXT_PUBLIC_API_KEY}`,
+    authDomain: `${process.env.NEXT_PUBLIC_AUTH_DOMAIN}`,
+    projectId: `${process.env.NEXT_PUBLIC_PROJECT_ID}`,
+    storageBucket: `${process.env.NEXT_PUBLIC_STORAGE_BUCKET}`,
+    messagingSenderId: `${process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.NEXT_PUBLIC_APP_ID}`,
+  });
 
 const httpLink = new HttpLink({
   uri: "/api/graphql",
@@ -51,18 +63,25 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <>
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
-    </Head>
-    <ApolloProvider client={client}>
-      <ModalProvider>
-      {getLayout(
-        <Component {...pageProps} />
-        )}
-      </ModalProvider>
-    </ApolloProvider>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <ApolloProvider client={client}>
+        <AuthProvider>
+          <ModalProvider>
+            {getLayout(<Component {...pageProps} />)}
+          </ModalProvider>
+        </AuthProvider>
+      </ApolloProvider>
     </>
   );
 }

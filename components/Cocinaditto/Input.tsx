7b25485388b474709030f6bf0480d30a12dfc38a
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-09 16:40:22 -0300

    feat: Add error and HTMLInputElement props

--- components/Cocinaditto/Input.tsx
+++ components/Cocinaditto/Input.tsx
@@ -1,21 +1,37 @@
-import { DetailedHTMLProps, FunctionComponent, InputHTMLAttributes } from "react"
+import {
+  DetailedHTMLProps,
+  FunctionComponent,
+  InputHTMLAttributes,
+} from "react";
 import { twMerge } from "tailwind-merge";
 
-interface IInputData extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
-    label?: string
+interface IInputData
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  label?: string;
+  error?: {
+    message?: string;
+  };
 }
 
 export const CocinadittoInput: FunctionComponent<IInputData> = ({
-    label,
-    type,
-    placeholder,
-    className,
-    min
+  label,
+  className,
+  error,
+  ...props
 }) => {
-    return (
-        <label className="flex flex-col items-center text-white">
-            {label}
-            <input type={type} placeholder={placeholder} className={twMerge("text-black rounded-full py-1 px-3 shadow-[inset_0px_4px_4px_rgba(0,0,0,0.25)]", className)} min={min} />
-        </label>
-    )
-}
+  return (
+    <label className="flex flex-col items-center text-white">
+      {label}
+      <input
+        {...props}
+        className={twMerge(
+          "text-black rounded-full py-1 px-3 shadow-[inset_0px_4px_4px_rgba(0,0,0,0.25)]",
+          className
+        )}
+      />
+    </label>
+  );
+};

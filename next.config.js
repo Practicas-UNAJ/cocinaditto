34f45be4527aead8ee0ec0639263e830718de045
01R4D-M <monterosrubendario@gmail.com>
2022-11-22 15:33:10 -0300

    chore: Add Firebase Storage image domain

--- next.config.js
+++ next.config.js
@@ -15,7 +15,8 @@ const nextConfig = {
         ]
       }
     ]
-  }
+  },   
+  images: { domains: ['firebasestorage.googleapis.com'], formats: ['image/avif', 'image/webp'], }
 }
 
 module.exports = nextConfig

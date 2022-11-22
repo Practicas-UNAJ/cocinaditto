833a82446922c823f748a86c57d79aca0e24aa47
01R4D-M <monterosrubendario@gmail.com>
2022-11-22 15:34:53 -0300

    refactor: Add user display and more buttons

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,10 +1,13 @@
 import { Icon } from "@iconify/react";
 import { FunctionComponent } from "react";
+import { twMerge } from "tailwind-merge";
 import { EModals } from "../enums/modals";
 import useAuth from "../hooks/useAuth";
 import useModal from "../hooks/useModal";
+import { UserDisplay } from "./UserDisplay";
 
 interface IMenuProps {
+  state: boolean
   setState: () => void;
 }
 
@@ -13,19 +16,19 @@ export const Menu: FunctionComponent<IMenuProps> = (props) => {
   const { currentUser } = useAuth();
 
   return (
-    <div className="fixed z-20 right-0 flex flex-col gap-3 h-screen p-12 text-brown-900 font-semibold gradient drop-shadow-2xl">
+    <div className={twMerge("fixed duration-300 z-20 right-0 flex flex-col gap-3 h-screen p-12 text-brown-900 font-semibold gradient drop-shadow-2xl", !props.state && "translate-x-full")}>
       <button className="absolute top-5 left-7" onClick={props.setState}>
         <Icon icon="ep:close-bold" />
       </button>
       {currentUser ? (
         <>
-          <button className="text-shadow">Tu perfil</button>
-          <button className="text-shadow">Editar perfil</button>
-          <button className="text-shadow">Publicar una receta</button>
-          <button className="text-shadow">Publicaciones destacadas</button>
-          <button className="text-shadow">Receta aleatoria</button>
-          <button className="text-shadow">Seguidos</button>
-          <button className="text-shadow">Cerrar sesión</button>
+          <div className="flex flex-col gap-3 h-full">
+            <button className="text-shadow">Publicar una receta</button>
+            <button className="text-shadow">Publicaciones destacadas</button>
+            <button className="text-shadow">Receta aleatoria</button>
+            <button className="text-shadow">Seguidos</button>
+          </div>
+          <UserDisplay/>
         </>
       ) : (
         <>

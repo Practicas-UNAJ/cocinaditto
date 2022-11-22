d731b0b7c5d4285416917c2d5c5df14eec843af7
01R4D-M <monterosrubendario@gmail.com>
2022-11-22 15:42:45 -0300

    refactor: adapt for any image style

--- components/Cocinaditto/ImageInput.tsx
+++ components/Cocinaditto/ImageInput.tsx
@@ -1,23 +1,14 @@
 import { NextComponentType } from "next";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { RecipeImage } from "../recipes/RecipeImage";
 import placeholder from "../../assets/images/recipe-placeholder.png";
 
-export const ImageInput: NextComponentType = () => {
-  const [imageData, setImageData] = useState<string>("");
-
+export const ImageInput = ({children}: any) => {
   return (
-    <div className="relative group">
-      {imageData !== "" ? (
-        <RecipeImage image="https://picsum.photos/1000" />
-      ) : (
-        <RecipeImage image={placeholder.src} />
-      )}
+    <div className="relative group object-cover">
+      {children}
       <div className="absolute top-0 left-0 grid items-center justify-center w-full h-full">
         <input type="file" className="absolute w-full h-full opacity-0" />
-        <span className="text-brown-700 font-semibold">
-          {imageData !== "" ? "" : "Publica una imagen de tu receta"}
-        </span>
       </div>
     </div>
   );

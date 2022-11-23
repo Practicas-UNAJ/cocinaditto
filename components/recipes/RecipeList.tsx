61d4fbf274edb77fecbc7365a1cff3c83f997957
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-23 00:38:11 -0300

    fix: Add key prop for element in iterator

--- components/recipes/RecipeList.tsx
+++ components/recipes/RecipeList.tsx
@@ -5,7 +5,7 @@ import { RandomRecipe } from "../RandomRecipe";
 import { HIGHLIGHTED, RecipeCard } from "./RecipeCard";
 
 interface IRecipeList {
-  list?: Recipe[]
+  list?: Recipe[];
 }
 
 export const RecipeList: FunctionComponent<IRecipeList> = (props) => {
@@ -13,15 +13,15 @@ export const RecipeList: FunctionComponent<IRecipeList> = (props) => {
     <>
       <div className="grid grid-cols-3 gap-3">
         {props.list?.slice(0, 6).map((recipe: Recipe, key: number) => (
-          <RecipeCard {...recipe} highlighted={HIGHLIGHTED.NONE} />
-          ))}
+          <RecipeCard {...recipe} key={key} />
+        ))}
       </div>
-      <RandomRecipe/>
-      <div className="grid grid-cols-3 gap-3">  
+      <RandomRecipe />
+      <div className="grid grid-cols-3 gap-3">
         {props.list?.slice(6).map((recipe: Recipe, key: number) => (
-          <RecipeCard {...recipe} highlighted={HIGHLIGHTED.NONE} />
-          ))}
+          <RecipeCard {...recipe} key={key} />
+        ))}
       </div>
     </>
-  )
-}
+  );
+};

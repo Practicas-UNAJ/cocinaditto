aeac15e21ea1126f3c5ff7c8436e2142148553b7
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-23 00:36:34 -0300

    refactor: Implement some fixes and better functionality on profile

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,69 +1,86 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import { MainLayout } from "../../components/layouts/MainLayout";
 import { UserImage } from "../../components/UserImage";
-import useAuth from "../../hooks/useAuth";
-import { NextPageWithLayout } from "../_app"
+import { NextPageWithLayout } from "../_app";
 import { Icon } from "@iconify/react";
-import { RecipeList } from "../../components/recipes/RecipeList";
-import { useQuery } from "@apollo/client";
-import { RecipesData, RecipesVars } from "../../apollo/types";
-import { RecipesQuery } from "../../apollo/queries";
 import { RecipeQuerySortBy, RecipeQuerySortOrder } from "../../apollo/enum";
 import ErrorImage, { ErrorImageType } from "../../components/ErrorImage";
 import LoadingSpinner, { SpinnerType } from "../../components/LoadingSpinner";
+import { useRouter } from "next/router";
+import useProfile from "../../hooks/useProfile";
+import useRecipes from "../../hooks/useRecipes";
+import { RecipeCard } from "../../components/recipes/RecipeCard";
 
 const Page: NextPageWithLayout = () => {
-    const { currentUser } = useAuth();
-    const {data, loading, error } = useQuery<
-      RecipesData,
-      RecipesVars
-    >(RecipesQuery, {
-      variables: {query: {
-        values: {
-          author: currentUser?.username,
+  const router = useRouter();
+  const {
+    user,
+    loading: profileLoading,
+    error: profileError,
+  } = useProfile(router.query.id as string);
+  const {
+    fetch,
+    recipes,
+    loading: recipesLoading,
+    error: recipesError,
+  } = useRecipes();
+
+  useEffect(() => {
+    if (user)
+      fetch({
+        variables: {
+          query: {
+            values: {
+              author: router.query.id as string,
+            },
+            sort: {
+              by: RecipeQuerySortBy.CREATED_ON,
+              order: RecipeQuerySortOrder.DESC,
+            },
+          },
         },
-        sort: {
-          by: RecipeQuerySortBy.CREATED_ON,
-          order: RecipeQuerySortOrder.DESC,
-        }
-      }}
-    });
+      });
+  }, [user]);
 
+  if (profileLoading) return <LoadingSpinner type={SpinnerType.LARGE} />;
+  if (profileError) return <ErrorImage type={ErrorImageType.LARGE} />;
 
-    return (
-      <>
-        <div className="flex flex-row justify-around gap-7">
-          <UserImage image={currentUser?.thumbnail} />
-          <div className="flex flex-col justify-around">
-            <span className="font-semibold text-xl">{currentUser?.username}</span>
-            <div className="flex flex-row items-center gap-3">
-              <Icon icon="bxs:user" className="w-7 h-7" />
-              <span>200 seguidores</span>
-            </div>
-            <div className="flex flex-row items-center gap-3">
-              <Icon icon="bx:world" className="w-7 h-7" />
-              <span>aca va un emoji</span>
-            </div>
-            <div className="flex flex-row items-center gap-3">
-              <Icon icon="bxs:note" className="w-7 h-7" />
-              <span>10 recetas</span>
-            </div>
+  return user ? (
+    <>
+      <div className="flex flex-row justify-around gap-7">
+        <UserImage image={user.thumbnail} />
+        <div className="flex flex-col justify-around">
+          <span className="font-semibold text-xl">{user.username}</span>
+          <div className="flex flex-row items-center gap-3">
+            <Icon icon="bxs:user" className="w-7 h-7" />
+            <span>200 seguidores</span>
+          </div>
+          <div className="flex flex-row items-center gap-3">
+            <Icon icon="bx:world" className="w-7 h-7" />
+            <span>aca va un emoji</span>
+          </div>
+          <div className="flex flex-row items-center gap-3">
+            <Icon icon="bxs:note" className="w-7 h-7" />
+            <span>10 recetas</span>
           </div>
         </div>
-        {error && <ErrorImage type={ErrorImageType.SMALL}/>}
-        {loading && <LoadingSpinner type={SpinnerType.SMALL}/>}
-        {data && <RecipeList list={data.results.recipes}/>}
-      </>
-
-    )
-}
+      </div>
+      {recipesError && <ErrorImage type={ErrorImageType.SMALL} />}
+      {recipesLoading && <LoadingSpinner type={SpinnerType.SMALL} />}
+      {recipes &&
+        recipes.map((recipe, key) => <RecipeCard {...recipe} key={key} />)}
+    </>
+  ) : (
+    <></>
+  );
+};
 
 Page.getLayout = function getLayout(page: ReactElement) {
-    return (
-      <>
-        <MainLayout>{page}</MainLayout>
-      </>
-    );
-  };
-  
-  export default Page;
+  return (
+    <>
+      <MainLayout>{page}</MainLayout>
+    </>
+  );
+};
+
+export default Page;

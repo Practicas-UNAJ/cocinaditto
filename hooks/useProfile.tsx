ab4103ccda83c6d13da2662dd3943c4d90129d80
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-23 00:34:20 -0300

    feat: Add useProfile custom hook

---
+++ hooks/useProfile.tsx
@@ -0,0 +1,27 @@
+import { useQuery } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { UserQuery } from "../apollo/queries";
+import { UserData, UserVars } from "../apollo/types";
+import { IUser } from "../interfaces/user";
+
+const useProfile = (id: string) => {
+  const [user, setUser] = useState<IUser | undefined>();
+
+  const { data, loading, error } = useQuery<UserData, UserVars>(UserQuery, {
+    variables: {
+      id,
+    },
+  });
+
+  useEffect(() => {
+    if (data?.user) setUser(data.user);
+  }, [data]);
+
+  return {
+    user,
+    loading,
+    error,
+  };
+};
+
+export default useProfile;

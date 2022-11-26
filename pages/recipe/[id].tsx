ac4f29f19d26d310855ae2d38e6c4087d57e387d
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-26 19:18:38 -0300

    refactor: Add RecipeContent & show buttons if is owner

--- pages/recipe/[id].tsx
+++ pages/recipe/[id].tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { ReactElement, useState } from "react";
+import { ReactElement } from "react";
 import { FlagCheckbutton } from "../../components/Cocinaditto/FlagCheckbutton";
 import { CocinadittoTitle } from "../../components/Cocinaditto/Title";
 import { MainLayout } from "../../components/layouts/MainLayout";
@@ -11,6 +11,7 @@ import useRecipePage from "../../hooks/useRecipePage";
 import { useRouter } from "next/router";
 import useLike from "../../hooks/useLike";
 import LoadingSpinner, { SpinnerType } from "../../components/LoadingSpinner";
+import RecipeContent from "../../components/RecipeContent";
 
 const Page: NextPageWithLayout = () => {
   const router = useRouter();
@@ -37,23 +38,23 @@ const Page: NextPageWithLayout = () => {
         {recipe.isVegan && <FlagCheckbutton image={veganIcon} state={true} />}
       </div>
 
-      <div className="bg-brown-500 font-light text-brown-900 p-4 rounded-2xl shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
-        {recipe.content}
-      </div>
+      <RecipeContent content={recipe.content} />
 
       <div className="flex flex-row text-brown-900 justify-center items-center gap-2">
         <button onClick={() => likeRecipe()}>{likeIcon}</button>
         {recipe.likes}
       </div>
 
-      <div className="flex flex-row gap-3 justify-center">
-        <button className="rounded-full font-semibold p-4 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-danger-600 text-white">
-          Eliminar
-        </button>
-        <button className="rounded-full font-semibold p-4 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-secondary-500 text-brown-900">
-          Editar
-        </button>
-      </div>
+      {recipe.isOwner && (
+        <div className="flex flex-row gap-3 justify-center">
+          <button className="rounded-full font-semibold p-4 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-danger-600 text-white">
+            Eliminar
+          </button>
+          <button className="rounded-full font-semibold p-4 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-secondary-500 text-brown-900">
+            Editar
+          </button>
+        </div>
+      )}
     </>
   ) : (
     <></>

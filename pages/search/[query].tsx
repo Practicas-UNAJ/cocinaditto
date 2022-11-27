336650677c3802dd20031a8457cb047148376443
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-26 23:52:59 -0300

    refactor: Add infinite scroll

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,52 +1,49 @@
-import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import { ReactElement } from "react";
 import { RecipeQuerySortBy, RecipeQuerySortOrder } from "../../apollo/enum";
-import { RecipesQuery } from "../../apollo/queries";
-import { RecipesData, RecipesVars } from "../../apollo/types";
 import ErrorImage, { ErrorImageType } from "../../components/ErrorImage";
 import { MainLayout } from "../../components/layouts/MainLayout";
 import LoadingSpinner, { SpinnerType } from "../../components/LoadingSpinner";
 import { RecipeList } from "../../components/recipes/RecipeList";
+import useRecipes from "../../hooks/useRecipes";
 import { NextPageWithLayout } from "../_app";
 
 const Page: NextPageWithLayout = () => {
-  const router = useRouter()
-  const { data, loading, error } = useQuery<
-    RecipesData,
-    RecipesVars
-  >(
-    RecipesQuery,
-    {
-      variables: {
-        query: {
-          values: {
-            title: router.query.query,
-          },
-          sort: {
-            by: RecipeQuerySortBy.CREATED_ON,
-            order: RecipeQuerySortOrder.DESC,
-          }
-        }
-      }
-    }
-  )
+  const router = useRouter();
+  const { fetchMore, hasMore, loading, recipes, error } = useRecipes();
+
+  const fetchMoreRecipes = () => {
+    fetchMore({
+      query: {
+        values: {
+          title: router.query.query as string,
+        },
+        sort: {
+          by: RecipeQuerySortBy.CREATED_ON,
+          order: RecipeQuerySortOrder.DESC,
+        },
+      },
+    });
+  };
+
   return (
     <>
-      { error && (
-        <>
-          <ErrorImage type={ErrorImageType.SMALL} />
-          {console.log(error)}
-        </>
+      {error && <ErrorImage type={ErrorImageType.SMALL} />}
+      {loading && <LoadingSpinner type={SpinnerType.SMALL} />}
+      {recipes && (
+        <RecipeList
+          fetchMore={fetchMoreRecipes}
+          hasMore={hasMore}
+          list={recipes}
+          loading={loading}
+        />
       )}
-      { loading && <LoadingSpinner type={SpinnerType.SMALL} /> }
-      {data && <RecipeList list={data.results.recipes}/>}
     </>
-  )
-}
+  );
+};
 
 Page.getLayout = function getLayout(page: ReactElement) {
   return <MainLayout>{page}</MainLayout>;
 };
-  
+
 export default Page;

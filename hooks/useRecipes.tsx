d689a056042c75a82f237bc5db03acec11242e15
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-26 23:52:19 -0300

    feat: Add fetchMore & hasMore

--- hooks/useRecipes.tsx
+++ hooks/useRecipes.tsx
@@ -4,20 +4,39 @@ import { RecipesQuery } from "../apollo/queries";
 import { RecipesData, RecipesVars } from "../apollo/types";
 import { Recipe } from "../modules/graphql/types/interfaces";
 
-const useRecipes = () => {
-  const [recipes, setRecipes] = useState<Recipe[] | undefined>();
-
+const useRecipes = (limit?: number) => {
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [fetch, { data, loading, error }] = useLazyQuery<
     RecipesData,
     RecipesVars
   >(RecipesQuery);
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
-    if (data?.results) setRecipes(data.results.recipes);
+    if (data?.results) {
+      setRecipes((state) => [...state, ...data.results.recipes]);
+      setHasMore(data.results.hasMore);
+    }
   }, [data]);
+
+  const fetchMore = ({ query }: RecipesVars) => {
+    fetch({
+      variables: {
+        query: {
+          ...query,
+          pagination: {
+            offset: recipes.length,
+            take: 6,
+          },
+        },
+      },
+    });
+  };
 
   return {
     fetch,
+    fetchMore,
+    hasMore,
     recipes,
     loading,
     error,

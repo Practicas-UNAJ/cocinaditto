f035ca445e4edc5adde53eb143827168df47bfae
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-26 23:47:38 -0300

    refactor: Add z-0

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "../Navbar";
 
 export const MainLayout = ({ children }: any) => {
   return (
-    <div className="flex flex-col items-center overflow-x-hidden font-poppins text-brown-900">
+    <div className="flex flex-col items-center overflow-x-hidden font-poppins text-brown-900 z-0">
       <Navbar />
       <section className="flex flex-col gap-10 p-5 w-screen sm:max-w-screen-sm">
         {children}

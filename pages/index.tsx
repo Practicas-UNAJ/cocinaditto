f2662364e07586837d2450df99e2cfe61168a13e
01R4D-M <monterosrubendario@gmail.com>
2022-11-27 21:33:04 -0300

    refactor: Add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,14 @@ import { RandomRecipe } from "../components/RandomRecipe";
 import { LastRecipes } from "../components/recipes/LastRecipes";
 import { NewRecipe } from "../components/NewRecipe";
 import ExhibitDisclaimer from "../components/ExhibitDisclaimer";
+import Head from "next/head";
 
 const Page: NextPageWithLayout = () => {
   return (
     <>
+      <Head>
+        <title>Cocinaditto | Inicio</title>
+      </Head>
       <Searchbar />
       <FeaturedRecipes />
       <RandomRecipe />

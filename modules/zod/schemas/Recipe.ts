558980c8bb7577fcab0ebb1a280a0c1e7d609ff1
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-28 23:49:03 -0300

    feat: Add editRecipe validation schema

--- modules/zod/schemas/Recipe.ts
+++ modules/zod/schemas/Recipe.ts
@@ -23,3 +23,44 @@ export const recipeSchema = z.object({
   isVegan: z.boolean({ required_error: zodMessages.EMPTY_FIELD }),
   glutenFree: z.boolean({ required_error: zodMessages.EMPTY_FIELD }),
 });
+
+export const editRecipeSchema = z.object({
+  title: z
+    .string({ required_error: zodMessages.EMPTY_FIELD })
+    .min(1, {
+      message: zodMessages.MIN_CHAR,
+    })
+    .optional(),
+  thumbnail: z
+    .string({ required_error: zodMessages.EMPTY_FIELD })
+    .min(1, {
+      message: zodMessages.MIN_CHAR,
+    })
+    .optional(),
+  country: z
+    .string({ required_error: zodMessages.EMPTY_FIELD })
+    .min(1, {
+      message: zodMessages.MIN_CHAR,
+    })
+    .optional(),
+  content: z
+    .string({ required_error: zodMessages.EMPTY_FIELD })
+    .min(1, {
+      message: zodMessages.MIN_CHAR,
+    })
+    .optional(),
+  portions: z
+    .string({ required_error: zodMessages.EMPTY_FIELD })
+    .min(1, {
+      message: zodMessages.MIN_CHAR,
+    })
+    .optional(),
+  cooking_time: z
+    .string({ required_error: zodMessages.EMPTY_FIELD })
+    .min(1, {
+      message: zodMessages.MIN_CHAR,
+    })
+    .optional(),
+  isVegan: z.boolean({ required_error: zodMessages.EMPTY_FIELD }).optional(),
+  glutenFree: z.boolean({ required_error: zodMessages.EMPTY_FIELD }).optional(),
+});

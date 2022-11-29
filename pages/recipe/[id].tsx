8691f83c4534a04dbda3564be45e81b532792621
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-28 23:50:19 -0300

    feat: Add edit & delete recipe functionality

--- pages/recipe/[id].tsx
+++ pages/recipe/[id].tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { ElementRef, ReactElement, useRef, useState } from "react";
+import { ElementRef, ReactElement, useEffect, useRef, useState } from "react";
 import { FlagCheckbutton } from "../../components/Cocinaditto/FlagCheckbutton";
 import { CocinadittoTitle } from "../../components/Cocinaditto/Title";
 import { MainLayout } from "../../components/layouts/MainLayout";
@@ -17,24 +17,83 @@ import ImageInput from "../../components/Cocinaditto/ImageInput";
 import { CocinadittoInput } from "../../components/Cocinaditto/Input";
 import { Icon } from "@iconify/react";
 import RichTextEditor from "../../components/RichTextEditor";
+import useModal from "../../hooks/useModal";
+import useDeleteRecipe from "../../hooks/useDeleteRecipe";
+import { EModals } from "../../enums/modals";
+import useForm from "../../hooks/useForm";
+import { editRecipeSchema } from "../../modules/zod/schemas/Recipe";
+import useEditRecipe from "../../hooks/useEditRecipe";
 
 type ImageInputHandle = ElementRef<typeof ImageInput>;
 
+interface IEditRecipeForm {
+  title: string;
+  thumbnail: string;
+  country: string;
+  content: string;
+  portions: string | number;
+  cooking_time: string | number;
+  isVegan: boolean;
+  glutenFree: boolean;
+}
+
 const Page: NextPageWithLayout = () => {
+  const { setModal, unsetModal } = useModal();
   const router = useRouter();
   const imageRef = useRef<ImageInputHandle>(null);
   const { recipe, isLoading } = useRecipePage(router.query.id as string);
-  const [ vegan, setVegan ] = useState<boolean>(false)
-  const [ glutenFree, setGlutenFree ] = useState<boolean>(false)
-  const [ infoState, setInfoState ] = useState<EInfoView>(
-    recipe?.isOwner ?
-    EInfoView.OWNER :
-    EInfoView.USER
-  )
+  const { deleteRecipe } = useDeleteRecipe(router.query.id as string);
+  const [infoState, setInfoState] = useState<EInfoView>();
+  const [ready, setReady] = useState(false);
+  const { form, errors, onChange, submit, updateForm } =
+    useForm<IEditRecipeForm>({ schema: editRecipeSchema });
+  const { editRecipe } = useEditRecipe(
+    { payload: form },
+    router.query.id as string
+  );
   const { icon: likeIcon, likeRecipe } = useLike(
     router.query.id as string,
     recipe?.likedByUser!
   );
+
+  useEffect(() => {
+    if (recipe) {
+      setInfoState(recipe.isOwner ? EInfoView.OWNER : EInfoView.USER);
+
+      updateForm({
+        content: recipe.content,
+        cooking_time: `${recipe.cooking_time}`,
+        country: recipe.country,
+        glutenFree: recipe.glutenFree,
+        isVegan: recipe.isVegan,
+        portions: `${recipe.portions}`,
+        thumbnail: recipe.thumbnail,
+        title: recipe.title,
+      });
+    }
+  }, [recipe]);
+
+  const submitWrapper = () => {
+    let parsedData: Partial<IEditRecipeForm> = {
+      cooking_time: parseInt(form.cooking_time as string),
+      portions: parseInt(form.portions as string),
+    };
+
+    updateForm({ ...form, ...parsedData });
+
+    if (imageRef.current?.imageDataURL)
+      updateForm({ thumbnail: imageRef.current.imageDataURL });
+
+    setReady(true);
+  };
+
+  useEffect(() => {
+    if (ready) {
+      editRecipe();
+
+      if (!errors) setInfoState(EInfoView.OWNER);
+    }
+  }, [ready]);
 
   if (isLoading) return <LoadingSpinner type={SpinnerType.LARGE} />;
 
@@ -43,20 +102,27 @@ const Page: NextPageWithLayout = () => {
       <Head>
         <title>Cocinaditto | {recipe.title}</title>
       </Head>
-
-      {
-        infoState === EInfoView.EDIT ?
+      {infoState === EInfoView.EDIT ? (
         <form className="flex flex-col gap-5">
           <CocinadittoTitle text="Editar receta" />
           <div className="flex flex-row gap-3 justify-center">
-            <button className="rounded-full w-12 h-12 p-3 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-danger-600">
-              <Icon
-                icon="bi:trash"
-                className="w-full h-full font-bold"
-              />
+            <button
+              type="button"
+              onClick={(ev) => {
+                setModal(ev, EModals.DELETE, {
+                  onAccept: () => {
+                    deleteRecipe();
+                    unsetModal();
+                  },
+                });
+              }}
+              className="rounded-full w-12 h-12 p-3 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-danger-600"
+            >
+              <Icon icon="bi:trash" className="w-full h-full font-bold" />
             </button>
             <button
-              onClick={() => setInfoState(EInfoView.OWNER)}
+              type="submit"
+              onClick={(ev) => submit({ ev, func: submitWrapper })}
               className="rounded-full w-12 h-12 p-3 shadow-[0px_4px_4px_rgba(0,0,0,0.25)]  bg-secondary-500"
             >
               <Icon
@@ -65,21 +131,30 @@ const Page: NextPageWithLayout = () => {
               />
             </button>
           </div>
-          <ImageInput ref={imageRef} type="RECIPE" />
+          <ImageInput
+            initialValue={recipe.thumbnail}
+            ref={imageRef}
+            type="RECIPE"
+            error={errors?.thumbnail}
+          />
           <CocinadittoInput
             className="w-full bg-brown-500 placeholder:text-brown-700"
             type="text"
             name="title"
-            placeholder="Nombre"
+            placeholder={recipe.title}
+            onChange={onChange}
+            error={errors?.title}
           />
           <div className="flex flex-row gap-5 items-center">
             <div className="w-1/3 flex flex-row gap-3 items-center">
               <Icon className="min-w-max" icon="bxs:time-five" />
               <CocinadittoInput
                 className="w-full bg-brown-500 placeholder:text-brown-700"
-                type="number"
-                placeholder="Tiempo"
+                type="text"
+                placeholder={`${recipe.cooking_time}`}
+                onChange={onChange}
                 name="cooking_time"
+                error={errors?.portions as string}
                 min={1}
               />
             </div>
@@ -88,18 +163,22 @@ const Page: NextPageWithLayout = () => {
               <CocinadittoInput
                 className="w-full bg-brown-500 placeholder:text-brown-700"
                 type="text"
-                placeholder="País o región"
+                placeholder={recipe.country}
+                onChange={onChange}
                 name="country"
+                error={errors?.country}
               />
             </div>
             <div className="w-1/3 flex flex-row gap-3 items-center">
               <Icon className="min-w-max" icon="fa6-solid:pizza-slice" />
               <CocinadittoInput
                 className="w-full bg-brown-500 placeholder:text-brown-700"
-                type="number"
-                placeholder="Porciones"
+                type="text"
+                placeholder={`${recipe.portions}`}
+                onChange={onChange}
                 min={1}
                 name="portions"
+                error={errors?.portions as string}
               />
             </div>
           </div>
@@ -107,64 +186,65 @@ const Page: NextPageWithLayout = () => {
           <div className="flex flex-row gap-5 justify-center">
             <FlagCheckbutton
               image={glutenFreeIcon}
-              state={glutenFree}
-              setState={() =>
-                setGlutenFree(!glutenFree)
-              }
+              state={form.glutenFree}
+              setState={() => updateForm({ glutenFree: !form.glutenFree })}
             />
             <FlagCheckbutton
               image={veganIcon}
-              state={vegan}
-              setState={() => setVegan(!vegan)}
+              state={form.isVegan}
+              setState={() => updateForm({ isVegan: !form.isVegan })}
             />
           </div>
 
           <RichTextEditor
-            cb={(content: string) => console.log("a")}
+            initialContent={form.content}
+            cb={(content: string) => updateForm({ ...form, content })}
+            error={errors?.content}
           />
         </form>
-      :
-      <>
-        <RecipeImage image={recipe.thumbnail} />
-        <CocinadittoTitle text={recipe.title} />
+      ) : (
+        <>
+          <RecipeImage image={recipe.thumbnail} />
+          <CocinadittoTitle text={recipe.title} />
 
-        <span className="text-center font-extralight">Publicado por: {recipe.author.username}</span>
+          <span className="text-center font-extralight">
+            Publicado por: {recipe.author.username}
+          </span>
 
-        {infoState === EInfoView.OWNER && (
-          <div className="flex flex-row gap-3 justify-center">
-            <button className="rounded-full w-12 h-12 p-3 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-danger-600">
-              <Icon
-                icon="bi:trash"
-                className="w-full h-full font-bold"
-              />
-            </button>
-            <button
-              onClick={() => setInfoState(EInfoView.EDIT)}
-              className="rounded-full w-12 h-12 p-3 shadow-[0px_4px_4px_rgba(0,0,0,0.25)]  bg-secondary-500"
-            >
-              <Icon
-                icon="bxs:pencil"
-                className="w-full h-full font-bold"
-              />
-            </button>
-          </div>
-        )}
-
-        <div className="flex flex-row gap-5 justify-center">
-          {recipe.glutenFree && (
-            <FlagCheckbutton image={glutenFreeIcon} state={true} />
+          {infoState === EInfoView.OWNER && (
+            <div className="flex flex-row gap-3 justify-center">
+              <button
+                onClick={(ev) => {
+                  setModal(ev, EModals.DELETE, {
+                    onAccept: deleteRecipe,
+                  });
+                }}
+                className="rounded-full w-12 h-12 p-3 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-danger-600"
+              >
+                <Icon icon="bi:trash" className="w-full h-full font-bold" />
+              </button>
+              <button
+                onClick={() => setInfoState(EInfoView.EDIT)}
+                className="rounded-full w-12 h-12 p-3 shadow-[0px_4px_4px_rgba(0,0,0,0.25)]  bg-secondary-500"
+              >
+                <Icon icon="bxs:pencil" className="w-full h-full font-bold" />
+              </button>
+            </div>
           )}
-          {recipe.isVegan && <FlagCheckbutton image={veganIcon} state={true} />}
-        </div>
 
-        <RecipeContent content={recipe.content} />
+          <div className="flex flex-row gap-5 justify-center">
+            <FlagCheckbutton image={glutenFreeIcon} state={recipe.glutenFree} />
+            <FlagCheckbutton image={veganIcon} state={recipe.isVegan} />
+          </div>
 
-        <div className="flex flex-row text-brown-900 justify-center items-center gap-2">
-          <button onClick={() => likeRecipe()}>{likeIcon}</button>
-          {recipe.likes}
-        </div>
-      </>
-    }
+          <RecipeContent content={recipe.content} />
+
+          <div className="flex flex-row text-brown-900 justify-center items-center gap-2">
+            <button onClick={() => likeRecipe()}>{likeIcon}</button>
+            {recipe.likes}
+          </div>
+        </>
+      )}
     </>
   ) : (
     <></>

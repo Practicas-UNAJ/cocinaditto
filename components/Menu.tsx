e325926045ff0b8d5825754ce7e2e90f829a2415
01R4D-M <monterosrubendario@gmail.com>
2022-11-29 17:44:04 -0300

    refactor: Change css

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -5,6 +5,7 @@ import { twMerge } from "tailwind-merge";
 import { EModals } from "../enums/modals";
 import useAuth from "../hooks/useAuth";
 import useModal from "../hooks/useModal";
+import { useWindowSize } from "../hooks/useWindowsSize";
 import { UserDisplay } from "./UserDisplay";
 
 interface IMenuProps {
@@ -15,20 +16,24 @@ interface IMenuProps {
 export const Menu: FunctionComponent<IMenuProps> = (props) => {
   const { setModal } = useModal();
   const { currentUser } = useAuth();
+  const { width, height } = useWindowSize()
 
   const redirectTo = (endpoint: string) => Router.push(endpoint);
 
   return (
     <div
       className={twMerge(
-        "fixed duration-300 z-20 right-0 flex flex-col gap-3 h-screen p-12 text-brown-900 font-semibold gradient drop-shadow-2xl",
+        "fixed duration-300 z-30 right-0 flex flex-col gap-3 h-screen p-12 text-brown-900 font-semibold gradient drop-shadow-2xl",
         !props.state && "translate-x-full"
       )}
+      style={{
+        height: `${height}px`
+      }}
     >
       <button className="absolute top-5 left-7" onClick={props.setState}>
         <Icon icon="ep:close-bold" />
       </button>
-      {currentUser ? (
+      {!currentUser ? (
         <>
           <div className="flex flex-col gap-3 h-full">
             <button

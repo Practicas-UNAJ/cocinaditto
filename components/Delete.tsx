ea66208d0077f1e981b957e2a94db70930d36b09
Fabricio Futryk <ffutryk52@gmail.com>
2022-11-28 23:48:01 -0300

    feat: Add DeleteModal functionality

--- components/Delete.tsx
+++ components/Delete.tsx
@@ -1,26 +1,35 @@
-import { NextComponentType } from "next";
-import { MutableRefObject } from "react";
+import { FunctionComponent, MutableRefObject } from "react";
 import useModal from "../hooks/useModal";
 import useOutsideClick from "../hooks/useOutsideClick";
 
-export const Delete: NextComponentType = () => {
+export interface IDeleteProps {
+  onAccept: (...args: any) => any;
+}
+
+export const Delete: FunctionComponent<IDeleteProps> = ({ onAccept }) => {
   const { unsetModal } = useModal();
   const ref = useOutsideClick(unsetModal) as MutableRefObject<HTMLDivElement>;
 
   return (
     <div
       ref={ref}
-      className="flex flex-col items-center max-w-xs text-center card-gradient rounded-2xl py-3 text-brown-900 font-semibold"
+      className="flex flex-col items-center max-w-xs text-center gradient rounded-2xl py-3 text-brown-900 font-semibold"
     >
       <span>
         Estás por eliminar una receta, esta acción no puede revertirse.
       </span>
       <span>¿Quieres continuar de todos modos?</span>
       <div className="flex flex-row gap-2">
-        <button className="rounded-full p-3 bg-danger-800 text-white shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+        <button
+          onClick={onAccept}
+          className="rounded-full p-3 bg-danger-800 text-white shadow-[0px_4px_4px_rgba(0,0,0,0.25)]"
+        >
           Eliminar
         </button>
-        <button className="rounded-full p-3 border-2 border-brown-900">
+        <button
+          onClick={unsetModal}
+          className="rounded-full p-3 border-2 border-brown-900"
+        >
           Cancelar
         </button>
       </div>
